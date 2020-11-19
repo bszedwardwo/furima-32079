09db620afb62ceb452287ceb6837a334e406e71b
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-19 20:27:14 +0900

    新規登録からログアウトまでのルーティング設定、新規登録のバリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
+
+
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+  with_options presence: true, format: { with: /\A一-龥]+\z/, message: '漢字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\Aア-ン]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: '英字と数字の両方を含めて設定してください' }
+
+end
