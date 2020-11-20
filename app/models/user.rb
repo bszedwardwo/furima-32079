ea61852d879f6a9465b4746e00dfa732ae7acfb0
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-20 18:43:42 +0900

    Userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :validatable
 
 
   validates :nickname, presence: true
   validates :birth_date, presence: true
-  with_options presence: true, format: { with: /\A[あ-んア-ン一-龥]+\z/, message: '漢字を使用してください' } do
+  with_options presence: true, format: { with: /\A[あ-んア-ン一-龥]+\z/ } do
     validates :first_name
     validates :last_name
   end
   
-  with_options presence: true, format: { with: /\A[ア-ン]+\z/, message: '全角カナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ア-ン]+\z/ } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i, message: '英字と数字の両方を含めて設定してください' }
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,10 +5,10 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation {password}
-    last_name             {Faker::gimei.last.kanji}
-    first_name            {Faker::gimei.first.kanji}
-    last_name_kana        {Faker::gimei.last.katakana}
-    first_name_kana       {Faker::gimei.first.katakana}
-    birth_date            
+    last_name             {gimei.last.kanji}
+    first_name            {gimei.first.kanji}
+    last_name_kana        {gimei.last.katakana}
+    first_name_kana       {gimei.first.katakana}
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2010-01-01')}
   end
 end
