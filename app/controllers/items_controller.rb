dd0cd3d6a653a9fecc05be0e1f38aa2138643ead
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-22 16:17:26 +0900

    itemコントローラーのcreateアクションの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new
+    if @item.save
+      redirect_to root_path
+    else
+      @item = @item
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :text, :price,:category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id )
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
-
-  validates :title, :text, :price,:category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, presence: true
   belongs_to :user
 
+  validates :title, :text, :price,:category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, presence: true
 end
