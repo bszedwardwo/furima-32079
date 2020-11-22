e680728edbc3ea574d42329d87874e8cc384a1e2
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-22 16:27:53 +0900

    Revert "Revert "Active Storageの導入""
    This reverts commit ec434ab795d6e4ebc0861415808cdd76d5ace460.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
   belongs_to :user
+  has_one_attached :image
 
   validates :title, :text, :price,:category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, presence: true
 end
