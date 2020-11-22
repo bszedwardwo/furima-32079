3218264e206c19fc59a95e7798c82920b1a07eac
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-22 16:10:58 +0900

    activehashのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture
+  belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
 
+  validates :title, :text, :price,:category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, presence: true
   belongs_to :user
 
 end
