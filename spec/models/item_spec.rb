c33dfd9ee0b0183c864725a7c58743ea9b092471
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-23 14:45:15 +0900

    itemモデルのFaker設定

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  context '新規商品出品ができる時' do
+    it '' do
+      
+    end
 end
