8de1b881b114f3c7a4543427626006fc3ab3aec0
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-23 15:05:38 +0900

    itemモデルの異常系の条件設定

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,34 +22,46 @@ RSpec.describe Item, type: :model do
     end
   end
   context '新規商品出品ができない時' do
-    it '' do
+    it '商品画像がない時' do
 
     end
-    it '' do
+    it '商品名が空の時' do
       
     end
-    it '' do
+    it '商品名が41文字以上の時' do
       
     end
-    it '' do
+    it '商品の説明が空の時' do
       
     end
-    it '' do
+    it '商品の説明が1001文字以上の時' do
       
     end
-    it '' do
+    it '商品のカテゴリーが空の時' do
+
+    end
+    it '商品の状態が空の時' do
       
     end
-    it '' do
+    it '配送料の負担について情報が空の時' do
       
     end
-    it '' do
+    it '発送元の地域についての情報が空の時' do
       
     end
-    it '' do
+    it '発送までの日数についての情報が空の時' do
       
     end
-    it '' do
+    it '価格が空の時' do
+      
+    end
+    it '価格が300より少ない時' do
+      
+    end
+    it '価格が9999999より多い時' do
+      
+    end
+    it '価格が半角数字じゃない時' do
       
     end
   end
