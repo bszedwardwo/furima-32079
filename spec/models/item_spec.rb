22354ca763addc2bb65352a5462eed39c0d3b9ef
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-23 15:45:08 +0900

    itemモデルの異常系のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,46 +23,77 @@ RSpec.describe Item, type: :model do
   end
   context '新規商品出品ができない時' do
     it '商品画像がない時' do
-
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
     end
     it '商品名が空の時' do
-      
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
     end
     it '商品名が41文字以上の時' do
-      
+      @item.name = 'adddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafo'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
     end
     it '商品の説明が空の時' do
-      
+      @item.info = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Info can't be blank")
     end
     it '商品の説明が1001文字以上の時' do
-      
+      @item.info = 'adddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoaddddddddd
+      ddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgn
+      adddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafo
+      eihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafoauojeaoiughaouhfojafoadddddddddddddeiofajioejjjjafoeihgnauojeaoiughaouhfojafo'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Info is too long (maximum is 1000 characters)")
     end
     it '商品のカテゴリーが空の時' do
-
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
     end
     it '商品の状態が空の時' do
-      
+      @item.sales_status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sales status can't be blank")
     end
     it '配送料の負担について情報が空の時' do
-      
+      @item.sales_status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Sales status can't be blank")
     end
     it '発送元の地域についての情報が空の時' do
-      
+      @item.prefecture_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
     end
     it '発送までの日数についての情報が空の時' do
-      
+      @item.scheduled_delivery_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
     end
     it '価格が空の時' do
-      
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
     end
     it '価格が300より少ない時' do
-      
+      @item.price = '299'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than 300")
     end
     it '価格が9999999より多い時' do
-      
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than 9999999")
     end
     it '価格が半角数字じゃない時' do
-      
+      @item.price = '渡辺'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
     end
   end
 end
