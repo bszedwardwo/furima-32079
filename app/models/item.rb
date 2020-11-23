57819c5aca197f2b1ca3540a5ec384fc182b7d6e
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-23 14:51:46 +0900

    itemテーブルのnameとinfoカラムの、文字数制限のバリデーション漏れを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,6 @@ class Item < ApplicationRecord
 
   validates :name, :info, :price, :image, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, presence: true
   validates :price, numericality: { only_integer: true , greater_than: 300, less_than: 9999999 }
+  validates :name, length: { maximum: 40 }
+  validates :info, length: { maximum: 1000 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
   context '新規商品出品ができる時' do
-    it '' do
+    it '必要な項目が全て埋まっていれば登録できる' do
+
+    end
+    it '商品名が40文字いないなら登録できる' do
       
     end
+    it '商品説明が1000文字いないなら登録できる' do
+      
+    end
+    it '値段が300~9999999の間なら登録できる' do
+      
+    end
+  end
 end
