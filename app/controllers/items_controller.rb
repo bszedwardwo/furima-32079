bf7a63cb77ed55475f2a5611c6fffb8d379d5181
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-24 21:55:11 +0900

    rubocopによる自動整形

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-      unless current_user.id == @item.user_id
-        redirect_to root_path
-      end
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -37,9 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    unless current_user.id == @item.user_id
-      render :show
-    end
+    render :show unless current_user.id == @item.user_id
     if @item.destroy
       redirect_to root_path
     else
