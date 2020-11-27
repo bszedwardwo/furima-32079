1b5ee96bd6db1f3abcd77e08ea00d89c8ae0ccc0
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-27 12:07:32 +0900

    paymentsテーブルのマイグレーション

--- db/migrate/20201127030245_create_payments.rb
+++ db/migrate/20201127030245_create_payments.rb
@@ -1,7 +1,8 @@
 class CreatePayments < ActiveRecord::Migration[6.0]
   def change
     create_table :payments do |t|
-
+      t.references :item, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
