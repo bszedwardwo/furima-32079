bc65bc16cc20576aa610142f43cc3174366007e4
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-27 12:00:05 +0900

    値段が空でもJavaScriptを読み込んでしまう問題を修正

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,15 +1,16 @@
 function tax() {
   const itemPrice = document.getElementById("item-price");
-  
-  itemPrice.addEventListener("keyup", () => {
-    const itemTax = itemPrice.value;
-    const taxRate = 0.1;
-    const taxPrice = itemTax * taxRate;
-    const addTaxPrice = document.getElementById("add-tax-price");
-    const profit = document.getElementById("profit");
-    addTaxPrice.innerHTML = `${ taxPrice.toFixed() }`;
-    profit.innerHTML = `${ (itemTax - taxPrice).toFixed() }`;
-  } );
+  if (itemPrice != null) {
+    itemPrice.addEventListener("keyup", () => {
+      const itemTax = itemPrice.value;
+      const taxRate = 0.1;
+      const taxPrice = itemTax * taxRate;
+      const addTaxPrice = document.getElementById("add-tax-price");
+      const profit = document.getElementById("profit");
+      addTaxPrice.innerHTML = `${ taxPrice.toFixed() }`;
+      profit.innerHTML = `${ (itemTax - taxPrice).toFixed() }`;
+    });
+  };
 };
 
 window.addEventListener("load", tax);
