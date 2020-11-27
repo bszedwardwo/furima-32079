53a77dc47bb22c3e39517ee0546676d7da5e26c3
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-27 12:26:59 +0900

    paymentとpayment_infomationテーブルのバリデーション設定

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,6 @@
 class Payment < ApplicationRecord
   belongs_to :item, :user
   has_one    :payment_infomation
+
+  validates :item, :user, presence: true
 end
--- app/models/payment_infomation.rb
+++ app/models/payment_infomation.rb
@@ -1,3 +1,5 @@
 class PaymentInfomation < ApplicationRecord
   belongs_to :payment
+
+  validates :postal_code, :prefecture_id, :city, :address, :phone_number, :payment, presence: true
 end
