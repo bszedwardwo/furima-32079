6f2fe6a28b5ea57bb0c049898d4b6bbcdb456c7e
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-29 15:33:29 +0900

    購入情報をデータベースに保存できるよう設定

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,21 +1,25 @@
 class PaymentsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @item_payment = ItemPayment.new()
   end
 
   def create
-    binding.pry
-    payment = Payment.create(payment_params)
-    PaymentInfomation.create(payment_infomation_params(payment))
+    @item_payment = ItemPayment.new(item_payment_params)
+    if @item_payment.valid?
+      @item_payment.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      @item_payment = ItemPayment.new()
+      render :index
+    end
   end
 
   private
 
-  def payment_params
-    params.require(:payment).permit(:item_id , :user_id).merge(item_id: item.id, user_id: user.id)
+  def item_payment_params
+    params.require(:item_payment).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
-  def payment_infomation_params
-    params.require(:payment_infomation).parmit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
-  end
 end
---
+++ app/models/Item_payment.rb
@@ -0,0 +1,12 @@
+class ItemPayment
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
+
+  validates :postal_code, :prefecture_id, :city, :address, :phone_number, presence: true
+
+  def save
+    payment = Payment.create(user_id: user_id, item_id: item_id)
+    PaymentInfomation.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, payment_id: payment.id)
+  end
+
+end
