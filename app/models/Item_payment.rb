d9f4f105838d19e951f2ecac30c774a84efca539
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-30 11:47:16 +0900

    Revert "Revert "Update Item_payment.rb""
    This reverts commit 1f64c9becf2a0195ec4b14639f94bfb1056d72d3.

--- app/models/Item_payment.rb
+++ app/models/Item_payment.rb
@@ -2,8 +2,13 @@ class ItemPayment
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id, :token
 
-  validates :postal_code, :prefecture_id, :city, :address, :phone_number, presence: true
+  validates :prefecture_id, :city, :address, presence: true
   validates :token, presence: true
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input 10 or 11 number" }
+  end
 
   def save
     payment = Payment.create(user_id: user_id, item_id: item_id)
