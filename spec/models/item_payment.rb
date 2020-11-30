b24a99a5468e205d265352d4ace65a2fea775baa
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-11-30 17:36:51 +0900

    itempaymentモデルの異常系のテストコードの要件定義

--- spec/models/item_payment.rb
+++ spec/models/item_payment.rb
@@ -13,4 +13,30 @@ RSpec.describe ItemPayment, type: :model do
       expect(@itempayment).to be_valid
     end
   end
+  context 'データベースに保存できない時' do
+    it 'tokenが空だと保存できない' do
+      
+    end
+    it 'postal_codeが空だと保存できない' do
+      
+    end
+    it 'postal_codeにハイフンがないと保存できない' do
+      
+    end
+    it 'prefectureが空だと保存できない' do
+      
+    end
+    it 'cityが空だと保存できない' do
+      
+    end
+    it 'addressが空だと保存できない' do
+      
+    end
+    it 'phone_numberが空だと保存できない' do
+      
+    end
+    it 'phone_numberが10or11桁以外だと保存できない' do
+      
+    end
+  end
 end
